1ad0643c3a5c81aad58cb5fcbf5e5088e2ab514c
Sena543 <senanuazanu@gmail.com>
2020-05-09 01:06:51 +0000

    Fixed the modal for the sign up and forgot password

--- screens/Loginscreen.js
+++ screens/Loginscreen.js
@@ -1,26 +1,49 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import { View, Text, TextInput, Button, Image, Modal} from 'react-native';
-import SignUp from './Signup'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
+import SignUp from './Signup';
+import ForgotPassword from './ForgotPassword'
+
+export const SignUpContext = createContext();
+export const ForgotPassContext = createContext();
 
 export default function Login(){
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [signupModalVisible, setsignupModalVisible] = useState(false)
     const [forgetModalVisible, setforgetModalVisible] = useState(false)
 
 
-    const Mod = ()=>{
+
+    const SignUpModal = ()=>{
         return(
             <Modal
             animationType="slide"
             visible={signupModalVisible}
             >
-                <SignUp signupModalVisible={signupModalVisible}/>
+                <SignUpContext.Provider value={{signupModalVisible, setsignupModalVisible}}>
+                    <SignUp />
+                </SignUpContext.Provider>
             </Modal>
         )
     }
+
+    const ForgotPasswordModal = ()=>{
+        return(
+            <Modal
+            animationType="slide"
+            visible={forgetModalVisible}
+            >
+                <ForgotPassContext.Provider value={{forgetModalVisible, setforgetModalVisible}}>
+                    <ForgotPassword />
+                </ForgotPassContext.Provider>
+            </Modal>
+        )
+    }
+
+    
 
     return(
         <View style={{flex:1}}>
@@ -35,14 +58,14 @@ export default function Login(){
             </View>
             <View style={{flexDirection:'row',alignItems:'center', justifyContent:'center', marginTop:20 }}>
                 <TextInput
-                 style={{ height: 45, width:300, borderColor: 'gray', borderWidth: .2 }}
+                 style={{ height: 45, width:320, borderColor: '#1c1c1c', borderWidth: .2, borderRadius:20 }}
                  onChangeText={text => setEmail(text)}
                  placeholder='Email'
                 />
             </View>
             <View style={{flexDirection:'row',alignItems:'center', justifyContent:'center', marginTop:30 }}>
                 <TextInput
-                 style={{ height: 45, width:300, borderColor: 'gray', borderWidth: .25 }}
+                 style={{ height: 45, width:320, borderColor: '#1c1c1c', borderWidth: .25, borderRadius:20 }}
                  onChangeText={text => setPassword(text)}
                  placeholder='Password'
                 />
@@ -50,16 +73,17 @@ export default function Login(){
             <View style={{marginLeft:90, marginRight:90, marginTop:30}}>
                 <Button
                 title='Sign In'
+                onPress = {()=> console.log(email, password)}
                 />
             </View>
             <View style={{alignItems:'center', justifyContent:'center',marginTop:30}}>  
                 <TouchableOpacity
                     underlayColor='#e3350e'
                     activeOpacity={.3}
-                    onPress={ ()=> setsignupModalVisible(false)}
-                >
+                    onPress={ ()=> setsignupModalVisible(true)}
+                    >
                     <Text>Sign Up</Text>
-                    <Mod/>
+                    <SignUpModal/>
                 </TouchableOpacity> 
             </View>
             
@@ -67,9 +91,10 @@ export default function Login(){
                 <TouchableOpacity
                     underlayColor='#e3350e'
                     activeOpacity={.3}
-                    onPress={ ()=> setforgetModalVisible(false)}
+                    onPress={ ()=> setforgetModalVisible(true)}
                 >
                     <Text>Forgot Password?</Text>
+                    <ForgotPasswordModal/>
                 </TouchableOpacity> 
             </View>
             
@@ -83,8 +108,15 @@ export default function Login(){
  * create the backend
  * replaace all image placehodlers with appropriate images
  * work on the headers
- * work on the displaying the modals for the forget password and sign up
  * add a splash
  * bonus
  * add animations
+ * 
+ * 
+ * ,
+    "splash": {
+      "image": "./assets/images/abstract.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#000000"
+    }
  */
